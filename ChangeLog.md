be547fdbf45d0e1810972adb64b61f02903aabe3
Spencer Mitchell <smitchell556@gmail.com>
2020-02-18 11:03:05 -0500

    Initial files

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for haskell-calculus-solver
+
+## Unreleased changes
---
+++ src/DataTypes.hs
@@ -0,0 +1,22 @@
+module DataTypes where
+
+data Expression
+  = Var String
+  | Int Int
+  | Negation Expression
+  | Sum      Expression Expression
+  | Sub      Expression Expression
+  | Product  Expression Expression
+  | Division Expression Expression
+  | Expt     Expression Expression
+  | Log      Expression Expression
+  | Func     String Expression
+  deriving (Eq, Show)
+
+data Derivation = Derivation String Expression
+
+-- Credit to Professor Sebastiaan Joosten. For use by calculus solver and
+-- arithmetic solver.
+data Calculation = Calc Expression [Step]
+data Step = Step LawName Expression
+data Law = Law String Expression Expression
